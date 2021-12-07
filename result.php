85de27f41089f7f460ee9aca99cf5441ae187968
EvgeniySelivanov <genyaselivanovzp@gmail.com>
2021-12-07 19:15:32 +0200

    final version

--- result.php
+++ result.php
@@ -20,20 +20,16 @@
 <div class="canvas_1">
     <canvas id="myChart" width="400" height="400"></canvas>
 </div>
+
 <script>
-
 let radio= "<?=$radio?>";
-
 let first= "<?=$first?>";
 let second= "<?=$second?>";
 let third= "<?=$third?>";
 let fourth= "<?=$fourth?>";
 const ctx = document.getElementById('myChart').getContext('2d');
+
 if(radio==1){
-
-
-
-
 const myChart = new Chart(ctx, {
     type: 'bar',
     data: {
@@ -66,128 +62,51 @@ const myChart = new Chart(ctx, {
         }
     }
 });
- }
-
-
-
-else{
-
-
-     /* document.write('какая-то диаграмма'); */
-     console.log('work');
-     
-     
-     
-     
-const DATA_COUNT = 5;
-const NUMBER_CFG = {count: DATA_COUNT, min: 0, max: 100};
-const data = {
-  labels: ['Red', 'Orange', 'Yellow', 'Green', 'Blue'],
-  datasets: [
-    {
-      label: 'Dataset 1',
-      data: Utils.numbers(NUMBER_CFG),
-      backgroundColor: Object.values(Utils.CHART_COLORS),
-    }
-  ]
-};
-
-const config = {
-  type: 'pie',
-  data: data,
-  options: {
-    responsive: true,
-    plugins: {
-      legend: {
-        position: 'top',
-      },
-      title: {
-        display: true,
-        text: 'Chart.js Pie Chart'
-      }
-    }
-  },
-};
-
-
-
-
-
-const actions = [
-  {
-    name: 'Randomize',
-    handler(chart) {
-      chart.data.datasets.forEach(dataset => {
-        dataset.data = Utils.numbers({count: chart.data.labels.length, min: 0, max: 100});
-      });
-      chart.update();
-    }
-  },
-  {
-    name: 'Add Dataset',
-    handler(chart) {
-      const data = chart.data;
-      const newDataset = {
-        label: 'Dataset ' + (data.datasets.length + 1),
-        backgroundColor: [],
-        data: [],
-      };
-
-      for (let i = 0; i < data.labels.length; i++) {
-        newDataset.data.push(Utils.numbers({count: 1, min: 0, max: 100}));
-
-        const colorIndex = i % Object.keys(Utils.CHART_COLORS).length;
-        newDataset.backgroundColor.push(Object.values(Utils.CHART_COLORS)[colorIndex]);
-      }
-
-      chart.data.datasets.push(newDataset);
-      chart.update();
-    }
-  },
-  {
-    name: 'Add Data',
-    handler(chart) {
-      const data = chart.data;
-      if (data.datasets.length > 0) {
-        data.labels.push('data #' + (data.labels.length + 1));
-
-        for (let index = 0; index < data.datasets.length; ++index) {
-          data.datasets[index].data.push(Utils.rand(0, 100));
-        }
-
-        chart.update();
-      }
-    }
-  },
-  {
-    name: 'Remove Dataset',
-    handler(chart) {
-      chart.data.datasets.pop();
-      chart.update();
-    }
-  },
-  {
-    name: 'Remove Data',
-    handler(chart) {
-      chart.data.labels.splice(-1, 1); // remove the label first
-
-      chart.data.datasets.forEach(dataset => {
-        dataset.data.pop();
-      });
-
-      chart.update();
-    }
-  }
-];
-
-
-
 }
 
 
 
+else{
+     console.log('work');        
+     const myChart = new Chart(ctx, {
+    type: 'pie',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green'],
+        datasets: [{
+            label: 'Selivanov charts!',
+            data: [first, second, third,fourth],
+            backgroundColor: [
+                'rgb(255, 99, 132)',
+                'rgb(54, 162, 235)',
+                'rgb(255, 206, 86)',
+                'rgb(75, 192, 192)'
+                
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)'
+                
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
 
- 
+
+
+
+  }
+
+
 
 </script>
     
